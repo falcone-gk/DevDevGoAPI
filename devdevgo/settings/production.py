471bb091c65c9925f709e4920fbeb9f346556f25
Enzo Falcón <falcone134@gmail.com>
2021-12-05 01:30:04 -0500

    Setting debug to false

--- devdevgo/settings/production.py
+++ devdevgo/settings/production.py
@@ -6,7 +6,7 @@ from .base import *
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
