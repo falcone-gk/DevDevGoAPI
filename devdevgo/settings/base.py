4bc7bf151dd9d2c8b867a1c0ef9e3b8c5a344504
Enzo Falcón <falcone134@gmail.com>
2021-12-05 01:25:56 -0500

    Adding whitenoise to handle styles

--- devdevgo/settings/base.py
+++ devdevgo/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
