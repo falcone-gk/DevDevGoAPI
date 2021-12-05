10a52e0a0056cc51e8033eb880c236a772237646
Enzo Falcón <falcone134@gmail.com>
2021-12-05 01:01:18 -0500

    Adding debugger

--- devdevgo/settings/production.py
+++ devdevgo/settings/production.py
@@ -4,6 +4,7 @@ from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
