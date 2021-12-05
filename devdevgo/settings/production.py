decb3284a8d192726132c0b8c120faba77fca1c8
Enzo Falcón <falcone134@gmail.com>
2021-12-05 01:08:42 -0500

    Set debug to true

--- devdevgo/settings/production.py
+++ devdevgo/settings/production.py
@@ -4,10 +4,9 @@ from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
