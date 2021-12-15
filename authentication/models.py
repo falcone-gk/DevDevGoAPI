918c075a6432ac2c9f0e1f193cf419aa401c33df
Enzo Falcón <falcone134@gmail.com>
2021-12-14 21:15:23 -0500

    Creating PreRegister model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class PreRegister(models.Model):
+
+    email = models.EmailField(unique=True ,max_length = 254)
+    activated = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.email
+
+    def is_activated(self):
+
+        return self.activated
